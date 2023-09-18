6b30800d78d50c21a85bf64e7d867581cd07ec4e
Ian Cossentino <ian.cossentino@cellsignal.com>
2023-09-18 14:50:36 -0400

    ci: update bowtie index locations; remove slurm cluster config

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,12 +7,12 @@ snakemake \
 --conda-frontend conda \
 --printshellcmds \
 --rerun-incomplete \
--j 50 \
---cluster-config cluster.yaml \
---cluster "sbatch -c {cluster.cpus} \
--t {cluster.time} -N {cluster.nodes} \
---mem {cluster.mem} \
---output {cluster.output} \
---error {cluster.error}" \
+-j 4 \
+# --cluster-config cluster.yaml \
+# --cluster "sbatch -c {cluster.cpus} \
+# -t {cluster.time} -N {cluster.nodes} \
+# --mem {cluster.mem} \
+# --output {cluster.output} \
+# --error {cluster.error}" \
 --cluster-cancel scancel \
 "$@"
